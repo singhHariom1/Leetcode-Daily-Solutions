6510d0791d47f4c681efe51b1292754d2442e8ff
Hariom Singh <singh.hariom6199@gmail.com>
2025-03-28 12:15:50 +0530

    move-zeroes28/03/2025

---
+++ move-zeroes.cpp
@@ -0,0 +1,60 @@
+// Brute Force Approach
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int n = nums.size(), index = 0;
+        vector<int> arr(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != 0)
+            {
+                arr[index++] = nums[i];
+            }
+        }
+        nums = arr;
+    }
+};
+
+// Optimized Approach
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int left = 0, right = 0, n = nums.size();
+        while (right < n)
+        {
+            if (nums[left] == 0 && nums[right] != 0)
+            {
+                swap(nums[left++], nums[right]);
+            }
+            else if (nums[left] != 0)
+            {
+                left++;
+            }
+            right++;
+        }
+    }
+};
+
+// Best Written Code
+
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int left = 0;
+        for (int right = 0; right < nums.size(); right++)
+        {
+            if (nums[right] != 0)
+            {
+                swap(nums[left++], nums[right]);
+            }
+        }
+    }
+};
